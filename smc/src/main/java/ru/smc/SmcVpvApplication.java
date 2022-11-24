71fcdb9dc3bf108fa07bfe3c32cb79d86d760645
Max <maxim.ketikov@gmail.com>
2022-11-24 18:02:58 +0300

    Первый коммит добавляет основные настройки бд, миграции, докера,

---
+++ smc/src/main/java/ru/smc/SmcVpvApplication.java
@@ -0,0 +1,13 @@
+package ru.smc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcVpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmcVpvApplication.class, args);
+	}
+
+}
